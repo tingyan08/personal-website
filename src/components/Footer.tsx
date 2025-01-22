8cc04ad55b7c722f4d9cb86f61f90f5da58bb611
vinkc <56856866+tingyan08@users.noreply.github.com>
2025-01-22 23:12:26 +0800

    update icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faResearchgate, faGoogleScholar, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import '../assets/styles/Footer.scss'
 
 function Footer() {
@@ -8,22 +11,24 @@ function Footer() {
     <footer>
       <div>
         <a href="mailto:tingyan@caece.net" target="_blank" rel="noreferrer">
-          <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
-          </svg>
+          <FontAwesomeIcon icon={faEnvelope} className="fa"/>
         </a>
-        <a href="https://github.com/tingyan08" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+
+        <a href="https://github.com/tingyan08" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} className="fa"/>
+        </a>
+
         <a href="https://scholar.google.com.tw/citations?user=t8dhlSQAAAAJ&hl=zh-TW" target="_blank" rel="noreferrer">
-          <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M390.9 298.5c0 0 0 .1 .1 .1c9.2 19.4 14.4 41.1 14.4 64C405.3 445.1 338.5 512 256 512s-149.3-66.9-149.3-149.3c0-22.9 5.2-44.6 14.4-64h0c1.7-3.6 3.6-7.2 5.6-10.7c4.4-7.6 9.4-14.7 15-21.3c27.4-32.6 68.5-53.3 114.4-53.3c33.6 0 64.6 11.1 89.6 29.9c9.1 6.9 17.4 14.7 24.8 23.5c5.6 6.6 10.6 13.8 15 21.3c2 3.4 3.8 7 5.5 10.5zm26.4-18.8c-30.1-58.4-91-98.4-161.3-98.4s-131.2 40-161.3 98.4L0 202.7 256 0 512 202.7l-94.7 77.1z"/>
-          </svg>
+          <FontAwesomeIcon icon={faGoogleScholar} className="fa"/>
         </a>
+
         <a href="https://www.researchgate.net/profile/Ting-Yan-Wu" target="_blank" rel="noreferrer">
-          <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M0 32v448h448V32H0zm262.2 334.4c-6.6 3-33.2 6-50-14.2-9.2-10.6-25.3-33.3-42.2-63.6-8.9 0-14.7 0-21.4-.6v46.4c0 23.5 6 21.2 25.8 23.9v8.1c-6.9-.3-23.1-.8-35.6-.8-13.1 0-26.1 .6-33.6 .8v-8.1c15.5-2.9 22-1.3 22-23.9V225c0-22.6-6.4-21-22-23.9V193c25.8 1 53.1-.6 70.9-.6 31.7 0 55.9 14.4 55.9 45.6 0 21.1-16.7 42.2-39.2 47.5 13.6 24.2 30 45.6 42.2 58.9 7.2 7.8 17.2 14.7 27.2 14.7v7.3zm22.9-135c-23.3 0-32.2-15.7-32.2-32.2V167c0-12.2 8.8-30.4 34-30.4s30.4 17.9 30.4 17.9l-10.7 7.2s-5.5-12.5-19.7-12.5c-7.9 0-19.7 7.3-19.7 19.7v26.8c0 13.4 6.6 23.3 17.9 23.3 14.1 0 21.5-10.9 21.5-26.8h-17.9v-10.7h30.4c0 20.5 4.7 49.9-34 49.9zm-116.5 44.7c-9.4 0-13.6-.3-20-.8v-69.7c6.4-.6 15-.6 22.5-.6 23.3 0 37.2 12.2 37.2 34.5 0 21.9-15 36.6-39.7 36.6z"/>
-          </svg>
+          <FontAwesomeIcon icon={faResearchgate} className="fa"/>
         </a>
-        <a href="https://www.linkedin.com/in/ting-yan-wu" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        
+        <a href="https://www.linkedin.com/in/ting-yan-wu" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} className="fa"/>
+        </a>
       </div>
       <p>A portfolio is designed & built based on the template provided by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a></p>
     </footer>
